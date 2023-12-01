64188bfc4be900d8fe19c3c678e65f1536f73eb5
21kyu <kyu21@outlook.com>
2023-12-01 16:12:57 +0000

    feat: implement get_cluster_cidr

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -5,19 +5,21 @@
 
 use std::env;
 
-use k8s_openapi::api::core::v1::Node;
+use k8s_openapi::api::core::v1::{ConfigMap, Node};
 use kube::{Api, Client};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
 
-    let host_ip = env::var("HOST_IP")?;
-    println!("host ip: {}", host_ip);
+    // let host_ip = env::var("HOST_IP")?;
+    // println!("host ip: {}", host_ip);
+
+    let context = Context::new().await?;
 
     let client = Client::try_default().await?;
 
-    let node_api: Api<Node> = Api::all(client);
+    let node_api: Api<Node> = Api::all(client.clone());
 
     // get all nodes
     let nodes = node_api.list(&Default::default()).await?;
@@ -31,13 +33,35 @@ async fn main() -> anyhow::Result<()> {
         println!("node name: {}", node_name);
         println!("node ip: {}", node_ip);
         println!("node pod cidr: {}", node_pod_cidr);
-        // get cluster cidr
-        let cluster_cidr = node_pod_cidr.split('.').collect::<Vec<&str>>()[0..3].join(".");
-        println!("cluster cidr: {}", cluster_cidr);
-        // get bridge ip
-        let bridge_ip = format!("{}.1", cluster_cidr);
-        println!("bridge ip: {}", bridge_ip);
     }
 
+    // get cluster cidr
+    let cluster_cidr = context.get_cluster_cidr().await?;
+    println!("cluster cidr: {}", cluster_cidr);
+
     Ok(())
+}
+
+struct Context {
+    client: Client,
+}
+
+impl Context {
+    async fn new() -> anyhow::Result<Self> {
+        let client = Client::try_default().await?;
+        Ok(Self { client })
+    }
+
+    async fn get_cluster_cidr(&self) -> anyhow::Result<String> {
+        let config_map_api: Api<ConfigMap> = Api::namespaced(self.client.clone(), "kube-system");
+
+        config_map_api
+            .get("kube-proxy")
+            .await?
+            .data
+            .and_then(|data| data.get("config.conf").cloned())
+            .and_then(|conf| serde_yaml::from_str::<serde_yaml::Value>(&conf).ok())
+            .and_then(|yaml| yaml["clusterCIDR"].as_str().map(ToOwned::to_owned))
+            .ok_or_else(|| anyhow::anyhow!("failed to get cluster cidr"))
+    }
 }
